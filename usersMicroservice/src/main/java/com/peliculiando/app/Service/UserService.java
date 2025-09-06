bfc6d8f29bc0b4b0b717fb410e2a2454971a8792
JuanSeb8778 <juanseb8778@gmail.com>
2025-09-06 01:04:00 -0500

    Microservicio de users funcionando

---
+++ usersMicroservice/src/main/java/com/peliculiando/app/Service/UserService.java
@@ -0,0 +1,31 @@
+package com.peliculiando.app.Service;
+
+import com.peliculiando.app.Entity.User;
+import com.peliculiando.app.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
+
